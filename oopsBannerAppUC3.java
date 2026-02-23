46793025f713315b261dbc485dca282d290d1bdf
Aadarsh <aadarshthakur900@gmail.com>
2026-02-23 12:41:38 +0530

    printing oops using string join UC3

---
+++ oopsBannerAppUC3.java
@@ -0,0 +1,16 @@
+public class oopsBannerAppUC3 {
+    public static void main(String[] args) {
+
+        String pattern = String.join("\n",
+                "   ***      ***     ******     ******",
+                "  ** **    ** **    **   **    **   **",
+                " **   **  **   **   **   **    **   **",
+                " **   **  **   **   ******     ******",
+                " **   **  **   **   **         **",
+                "  ** **    ** **    **         **",
+                "   ***      ***     **         **"
+        );
+
+        System.out.println(pattern);
+    }
+}
